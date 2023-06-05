bf69e4073633bf1026e868580b37189a19a72d84
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-06-05 21:47:07 +0800

    Add files via upload

---
+++ Visual-Basic-get-and-display-time.vb
@@ -0,0 +1,6 @@
+Module Module1
+    Sub Main()
+        Dim currentTime As DateTime = DateTime.Now ' 获取当前时间
+        Console.WriteLine("Current time: " & currentTime) ' 打印当前时间
+    End Sub
+End Module
